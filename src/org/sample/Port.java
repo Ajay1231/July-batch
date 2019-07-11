4ae941e8141c46a3894b4c2b61234494062614c3
Ajay1231 <ajayabraham001@yahoo.com>
2019-07-11 12:01:39 +0530

    print all the data

---
+++ src/org/sample/Port.java
@@ -0,0 +1,19 @@
+package org.sample;
+import java.util.Scanner;
+public class Port {
+
+	public void num() {
+	}
+	public static void main(String[] args) {
+	Scanner s =new Scanner(System.in);
+	System.out.println(" Enter the number");
+	int num= s.nextInt();
+	if (num%2==0) {
+	System.out.println(" The number is even ");
+	}
+	else {
+	System.out.println(" The number is odd ");
+	}
+	}
+	}
+
